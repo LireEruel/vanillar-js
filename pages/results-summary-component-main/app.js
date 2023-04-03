1de9d7d446b623dedf7876e1f62e515e596810be
Seohee Jeong <jsh001505@ncsoft.com>
2023-04-03 17:45:10 +0900

    What I learned 추가

--- pages/results-summary-component-main/app.js
+++ pages/results-summary-component-main/app.js
@@ -2,12 +2,32 @@ class App {
   constructor($body) {
     this.$body = $body;
     this.render();
-    this.student_id = 0;
+    this.show_summary = true;
   }
   async render() {
     const $ul = document.querySelector("ul");
+
+    const $right_card_title = document.querySelector(
+      "#resultSummaryComponentMain > div.summary-side > h3"
+    );
+    this.renderSummary($ul, $right_card_title);
+    const $button = document.querySelector("button");
+    $button.addEventListener("click", () => {
+      this.show_summary = !this.show_summary;
+      if (this.show_summary) {
+        this.renderSummary($ul, $right_card_title);
+        $button.textContent = "Continue";
+      } else {
+        this.renderWhatILearned($ul, $right_card_title);
+        $button.textContent = "Back";
+      }
+    });
+  }
+
+  async renderSummary($ul, $title) {
+    $ul.innerHTML = "";
+    $title.textContent = "Summary";
     const scoreData = await this.getScoreData();
-    console.log(scoreData);
     for (const score of scoreData) {
       const $li = document.createElement("li");
       const $h4 = document.createElement("h4");
@@ -23,6 +43,24 @@ class App {
       $h4.textContent = score.category;
       $span.textContent = score.score;
       $h5.appendChild(document.createTextNode(" / 100"));
+    }
+  }
+
+  renderWhatILearned($ul, $title) {
+    $ul.innerHTML = "";
+    $title.textContent = "What I Learned";
+
+    const learned = [
+      "Fetch JSON file and use.",
+      "Set background with gradient.",
+      "Change inner component contents.",
+      "Append both the span and text node.",
+    ];
+
+    for (const learn of learned) {
+      const $li = document.createElement("li");
+      $li.textContent = learn;
+      $ul.appendChild($li);
     }
   }
 
