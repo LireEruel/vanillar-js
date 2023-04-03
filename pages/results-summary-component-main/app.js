4cac516cdf4ea7c52342b7d47c8c1ca9cfbeb175
Seohee Jeong <jsh001505@ncsoft.com>
2023-04-03 16:48:17 +0900

    js로 렌더링하기 성공

--- pages/results-summary-component-main/app.js
+++ pages/results-summary-component-main/app.js
@@ -5,10 +5,25 @@ class App {
     this.student_id = 0;
   }
   async render() {
-    const $summary_side = document.querySelector(".summary-side");
-    const $ul = document.createElement("ul");
+    const $ul = document.querySelector("ul");
     const scoreData = await this.getScoreData();
     console.log(scoreData);
+    for (const score of scoreData) {
+      const $li = document.createElement("li");
+      const $h4 = document.createElement("h4");
+      const $h5 = document.createElement("h5");
+      const $span = document.createElement("span");
+
+      $ul.appendChild($li);
+      $li.appendChild($h4);
+      $li.appendChild($h5);
+      $h5.appendChild($span);
+
+      $li.classList = "summary-box " + score.category.toLowerCase();
+      $h4.textContent = score.category;
+      $span.textContent = score.score;
+      $h5.appendChild(document.createTextNode(" / 100"));
+    }
   }
 
   async getScoreData() {
