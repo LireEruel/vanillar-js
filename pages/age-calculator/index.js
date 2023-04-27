06ec0083e9c34ffc1e8ac8a2b7a92356c4f830f8
LireEruel <jsh001505@gmail.com>
2023-04-27 16:43:30 +0900

    계산해서 text 적용

--- pages/age-calculator/index.js
+++ pages/age-calculator/index.js
@@ -11,13 +11,24 @@ class ageCalculator {
     let input_month = document.querySelector("#month").value;
     let input_year = document.querySelector("#year").value;
 
+    let $yearsValue = document.querySelector("#yearsValue");
+    let $monthsValue = document.querySelector("#monthsValue");
+    let $daysValue = document.querySelector("#daysValue");
     if (input_day && input_month && input_year) {
       const now = dayjs();
       now.format();
-      const inputDate = dayjs(`${input_year}-${input_month}-${input_day}`);
+      let inputDate = dayjs(`${input_year}-${input_month}-${input_day}`);
       const diffYears = now.diff(inputDate, "year"); // 날짜 차이 계산
       const diffMonths = now.diff(inputDate, "month") % 12;
-      const diffDays = now.diff(inputDate, "day") % 365;
+      if (inputDate.isBefore(now)) {
+        inputDate = inputDate.add(now.diff(inputDate, "month"), "month");
+      } else {
+        inputDate = inputDate.subtract(now.diff(inputDate, "month"), "month");
+      }
+      const diffDays = now.diff(inputDate, "day");
+      $yearsValue.textContent = diffYears;
+      $monthsValue.textContent = diffMonths;
+      $daysValue.textContent = diffDays;
     } else {
       console.log("날짜가 잘못됨.");
     }
