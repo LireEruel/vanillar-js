86d86e7a4b9624adfa9ba09a071e243cb5c5cfc6
LireEruel <jsh001505@gmail.com>
2023-04-28 14:05:06 +0900

    유효성 체크 추가

--- pages/age-calculator/index.js
+++ pages/age-calculator/index.js
@@ -4,13 +4,36 @@ class ageCalculator {
   }
   render() {
     this.$submitBtn = document.querySelector("#submitBtn");
-    this.$submitBtn.addEventListener("click", this.calculate);
+    this.$submitBtn.addEventListener("click", (e) => this.onClickSubmitBtn(e));
+    document
+      .querySelector("form")
+      .addEventListener("submit", (e) => this.onClickSubmitBtn(e));
   }
-  calculate() {
+  onClickSubmitBtn(e) {
+    e.preventDefault();
     let input_day = document.querySelector("#day").value;
     let input_month = document.querySelector("#month").value;
     let input_year = document.querySelector("#year").value;
+    if (this.validateDateInput(input_day, input_month, input_year)) {
+      this.calculate(input_day, input_month, input_year);
+    } else {
+      console.log("날짜가 잘못됨.");
+    }
+  }
+  validateDateInput(inputYear, inputMonth, inputDay) {
+    let valid = true;
+    const input_date = dayjs(`${inputYear}-${inputMonth}-${inputDay}`);
+    if (!input_date.isValid()) {
+      valid = false;
+      console.log("!input_date.isValid()");
+    } else if (!input_date.isBefore(dayjs())) {
+      console.log("input_date.isBefore(dayjs())");
+      valid = false;
+    }
+    return valid;
+  }
 
+  calculate(input_year, input_month, input_day) {
     let $yearsValue = document.querySelector("#yearsValue");
     let $monthsValue = document.querySelector("#monthsValue");
     let $daysValue = document.querySelector("#daysValue");
