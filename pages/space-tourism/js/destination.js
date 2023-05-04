562212f8a1b09b62a290b9aacd86f06005a7fbed
LireEruel <jsh001505@gmail.com>
2023-05-04 15:39:10 +0900

    destination tablet 추가

--- pages/space-tourism/js/destination.js
+++ pages/space-tourism/js/destination.js
@@ -5,16 +5,16 @@ class Destination {
   }
   async render() {
     this.destinations = await this.getData();
-    const $tabs = document.querySelector("ul");
+    const $tabs = document.querySelector("#tabs");
     this.destinations.forEach((destination, index) => {
       const $li = document.createElement("li");
       $li.textContent = destination.name;
       $li.addEventListener("click", () => {
-        this.setDestination(destination);
+        this.setDestination(destination, index);
       });
       $tabs.appendChild($li);
     });
-    this.setDestination(this.destinations[0]);
+    this.setDestination(this.destinations[0], 0);
   }
   async getData() {
     let data;
@@ -27,14 +27,14 @@ class Destination {
     }
     return data;
   }
-  async setDestination(destination) {
+  async setDestination(destination, currentIndex) {
     document.querySelector("h1").textContent = destination.name;
     document.querySelector("p").textContent = destination.description;
     document.querySelector("#distance").textContent = destination.distance;
     document.querySelector("#travel").textContent = destination.travel;
     document.querySelector("#targetImage").src = "../" + destination.images.png;
-    [...document.querySelector("ul").children].forEach((tab) => {
-      if (tab.textContent == destination.name) {
+    [...document.querySelector("#tabs").children].forEach((tab, index) => {
+      if (currentIndex == index) {
         tab.classList.add("selected");
       } else {
         tab.classList.remove("selected");
